d73a20df170623406c22e44acd46e6283152466d
Jun Chen <junchen07@gmail.com>
2019-08-14 20:13:34 -0400

    profile schema

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,6 +2,13 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 const User = require('../../models/User');
+const config = require('config');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const {
+    check,
+    validationResult
+} = require('express-validator');
 
 //@route    GET api/auth
 //@desc     Test route
@@ -18,6 +25,73 @@ router.get('/', auth, async (req, res) => {
     }
 });
 
+//@route    POST api/auth
+//@desc     Authenticate user & get token
+//@access   Public
+router.post('/', [
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password is required').exists()
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({
+            errors: errors.array()
+        });
+    }
+
+    //pull variables out of req.body
+    const {
+        email,
+        password
+    } = req.body;
+
+    try {
+        //see if user exists
+        let user = await User.findOne({
+            email
+        });
+
+        if (!user) {
+            return res.status(400).json({
+                errors: [{
+                    msg: 'Invalid credentials'
+                }]
+            });
+        }
+
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({
+                errors: [{
+                    msg: 'Invalid credentials'
+                }]
+            });
+        }
+
+
+        //return jsonwebtoken
+        // res.send('User resgistered');
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+        jwt.sign(payload, config.get('jwtToken'), {
+            //token expires in an hour
+            expiresIn: 3600
+        }, (err, token) => {
+            if (err) throw err;
+            res.json({
+                token
+            });
+        });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+});
 
 
 
