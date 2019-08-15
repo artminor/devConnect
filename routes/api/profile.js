65d7343911421f6edf7fbb9fff86523454cbe18c
Jun Chen <junchen07@gmail.com>
2019-08-14 23:17:03 -0400

    get all profiles

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -107,9 +107,20 @@ router.post('/',
             res.status(500).send('Server Error');
         }
 
-
     }
 );
 
+//@route    GET api/profile
+//@desc     get all profiles
+//@access   Public
+router.get('/', async (req, res) => {
+    try {
+        const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+        res.json(profiles);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
 
 module.exports = router;
