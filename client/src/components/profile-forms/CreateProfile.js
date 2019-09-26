1317e6669892bb5ef1bc22afd9c98371a647bee8
Jun Chen <junchen07@gmail.com>
2019-09-25 23:31:59 -0400

    create profile component

---
+++ client/src/components/profile-forms/CreateProfile.js
@@ -0,0 +1,41 @@
+import React, { Fragment, useState } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+const CreateProfile = props => {
+  const [formData, setFormData] = useState({
+    company: '',
+    website: '',
+    location: '',
+    status: '',
+    skills: '',
+    githubusername: '',
+    bio: '',
+    twitter: '',
+    facebook: '',
+    linkedin: '',
+    youtube: '',
+    instagram: ''
+  });
+
+  const {
+    company,
+    website,
+    location,
+    status,
+    skills,
+    githubusername,
+    bio,
+    twitter,
+    facebook,
+    linkedin,
+    youtube,
+    instagram
+  } = formData;
+
+  return <Fragment></Fragment>;
+};
+
+CreateProfile.propTypes = {};
+
+export default CreateProfile;
